869f41d5148825839571916e66c2dcca8769900c
NOUREDDINEGLB <noureddineglaibi@gmail.com>
2018-09-19 11:03:19 -0400

    Implementation of edge.il

--- projet/phase1/edge.jl
+++ projet/phase1/edge.jl
@@ -3,8 +3,8 @@ import Base.show
 abstract type AbstractEdge{T,C,D} end
 
 mutable struct Edge{T,C,D} <: AbstractEdge{T,C,D}
-    name::String
-    weight::T
+    nameedge::String
+    weightedge::T
     node1name::String
     node1data::C
     node2name::String
@@ -15,28 +15,28 @@ end
 # on présume que tous les edges dérivant d'AbstractEdge
 # posséderont des champs `name` et `weight`.
 
-"""Renvoie le nom du node1."""
-name(edge::AbstractEdge) = edge.node1name
-
-"""Renvoie le donnees du node1."""
-name(edge::AbstractEdge) = edge.node1data
-
-"""Renvoie le nom du node2."""
-name(edge::AbstractEdge) = edge.node2name
-
-"""Renvoie le donnees du node2."""
-name(edge::AbstractEdge) = edge.node2data
-
 """Renvoie le nom du edge."""
-name(edge::AbstractEdge) = edge.name
+nameedge(edge::AbstractEdge) = edge.nameedge
 
 """Renvoie les donnees contenues dans le edge."""
-weight(edge::AbstractEdge) = edge.weight
+weightedge(edge::AbstractEdge) = edge.weightedge
+
+"""Renvoie le nom du node1."""
+node1name(edge::AbstractEdge) = edge.node1name
+
+"""Renvoie le donnees du node1."""
+node1data(edge::AbstractEdge) = edge.node1data
+
+"""Renvoie le nom du node2."""
+node2name(edge::AbstractEdge) = edge.node2name
+
+"""Renvoie le donnees du node2."""
+node2data(edge::AbstractEdge) = edge.node2data
 
 """Affiche un edge"""
 function show(edge::AbstractEdge)
-    s = string("Node1 ", node1name(edge), "data: ", node1data(edge),
-    "Node2 ", node2name(edge), "data: ", node2data(edge),
-    "Edge ", name(edge), ", weight: ", weight(edge))
+    s = string("Node1: ", node1name(edge), " data: ", node1data(edge),
+    " Node2: ", node2name(edge), " data: ", node2data(edge),
+    " Edge ", nameedge(edge), ", weight: ", weightedge(edge))
     println(s)
 end
