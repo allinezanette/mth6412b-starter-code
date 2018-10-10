4a139b04e215534f559d4942c651ea835851b697
allinezanette <43012146+allinezanette@users.noreply.github.com>
2018-10-09 20:48:53 -0400

    main.jl phase1

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -1,22 +1,31 @@
-import Base.show
+file = "instances/stsp/bayg29.tsp"
 
-#=Please include the files needed for the main program using the following commands:
-include("projet/phase1/node.jl")
-include("projet/phase1/edge.jl")
-include("projet/phase1/graph.jl")
-include("projet/phase1/read_stsp.jl")=#
+nodes_init, edges_init = read_stsp(file)
 
-#=Reading the nodes in the bayg29.tsp=#
-Data_nodes=read_nodes(read_header("instances/stsp/bayg29.tsp"),"instances/stsp/bayg29.tsp")
-Data_edges=read_edges(read_header("instances/stsp/bayg29.tsp"),"instances/stsp/bayg29.tsp")
+nodes_init = sort(nodes_init)
 
-nod=[]
+nodes_graph = Array{Node{Vector{Float64}}, 1}()
+edges_graph = Array{Edge{Vector{Float64}}, 1}()
 
-for i in keys(Data_nodes)
-    node=Node(string(i),Data_nodes[i])
-    push!(nod,node)
+for node in nodes_init
+    push!(nodes_graph, Node(string(node[1]), node[2]))
 end
 
-#=Calling the Array containing the nodes and the Array containing the edges=#
-return nod
-return Data_edges
+nodes_graph = Array{Node{Vector{Float64}}, 1}()
+edges_graph = Array{Edge{Vector{Float64}}, 1}()
+
+for node in nodes_init
+    push!(nodes_graph, Node(string(node[1]), node[2]))
+end
+
+Data_edges=read_edges(read_header(file),file)
+
+k=1
+for i=1:length(nodes_graph)
+    for j=i+1:length(nodes_graph)
+        push!(edges_graph,Edge([nodes_graph[Data_edges[k][1]],nodes_graph[Data_edges[k][2]]],Data_edges[k][3]))
+        k=k+1
+    end
+end
+G = Graph("Glaibi&Zanette", nodes_graph, edges_graph)
+show(G)
